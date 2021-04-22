33f6c6eb1ffaa1bfd07b823c5cbed3a109230b18
Anthony Alvarez <anscalva@ucsc.edu>
2021-04-22 03:54:06 -0700

    first commit

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,6 +7,7 @@ class Play extends Phaser.Scene {
     preload() {
         this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('mini', './assets/mini.png');
         this.load.image('starfield', './assets/starfield.png');
         this.load.spritesheet('explosion', './assets/explosion.png', 
         {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
@@ -20,9 +21,11 @@ this.add.rectangle(0, game.config.height - borderUISize, game.config.width, bord
 this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
 this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
 this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
-this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4.4, 'spaceship', 0, 30).setOrigin(0, 0);
 this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
 this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
+//mini mod insert//
+this.mini01 = new Mini(this, game.config.width, borderUISize + borderPadding + borderPadding*6, 'mini', 0, 100). setOrigin(0,0);
 keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
   keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
   keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
@@ -74,6 +77,7 @@ this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
         this.ship01.update();
      this.ship02.update();
 this.ship03.update();
+this.mini01.update();
         }
 if(this.checkCollision(this.p1Rocket, this.ship03)) {
   this.p1Rocket.reset();
@@ -86,6 +90,10 @@ if (this.checkCollision(this.p1Rocket, this.ship02)) {
 if (this.checkCollision(this.p1Rocket, this.ship01)) {
   this.p1Rocket.reset();
   this.shipExplode(this.ship01);
+}
+if (this.checkCollision(this.p1Rocket, this.mini01)) {
+  this.p1Rocket.reset();
+  this.shipExplode(this.mini01);
 }
       }
       // end of update //
