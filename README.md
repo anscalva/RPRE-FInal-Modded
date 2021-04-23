2567d2c0569f3d1ce752c9e44cba52827e3a0244
Anthony Alvarez <anscalva@ucsc.edu>
2021-04-23 03:27:47 -0700

    Current Point Total 50, plans to add more.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Remake 3 is really just an extension of 2 after there was a point the program brutally broke and I just downloaded 2's git into a new file and was too lazy to not try to fix 2's git so I simply made a new one. 
+RPRE- Modded is a work in progress currently. A student project to take the foundation of Rocket Patrol, and mod it based on criteria.
+
+simultaneous two-player mode (30) and Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+
+are the only two mods, with plans for a third 
+
+Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (20) 
+
+and potentially a fourth in 
+
+Implement a new timing/scoring mechanism that adds time to the clock for successful hits (20)
--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+// Anthony Alvarez
+// Rocket Patrol Modded 
+// 4/23/2021
+// 9 hours for base game tutorial finish
+// 2 hours for Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+// 6 hours for Implement a simultaneous two-player mode (30)
+
+// Total worktime 17 hours
+// Current Point Total: 50
+
+// Planned additional mods 
+// Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (20) 
+//  Display the time remaining (in seconds) on the screen (10)
+// Total planned point total 80
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
@@ -5,6 +20,6 @@ let config = {
     scene: [ Menu, Play ]
   }
 let game = new Phaser.Game(config);
-let keyF, keyR, keyLEFT, keyRIGHT;
+let keyF, keyR, keyLEFT, keyRIGHT, keyUP, keyA, keyS, keyD;
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
